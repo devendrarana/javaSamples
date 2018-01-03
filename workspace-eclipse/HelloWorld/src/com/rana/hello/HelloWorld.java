e48a856e2666a252a077355bcda050a34cd3906f
Devendra Rana <devendra50549rana@gmail.com>
2018-01-03 14:46:32 +0530

    Hello world project for java

---
+++ workspace-eclipse/HelloWorld/src/com/rana/hello/HelloWorld.java
@@ -0,0 +1,28 @@
+package com.rana.hello;
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author Devendra Rana
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hellow Java World !!");
+
+		Scanner scanner = new Scanner(System.in);
+		String  inputLine = scanner.nextLine();
+		
+		System.out.println("Your Input was = "+inputLine);
+		
+	}
+
+}
